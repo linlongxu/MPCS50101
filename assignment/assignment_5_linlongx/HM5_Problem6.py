58ff9ebe9e6d5238c8ededd8b78abb04e33831b2
linlongxu <simpson1291442489@gmail.com>
2024-11-06 18:25:08 -0600

    Delete assignment/assignment_5_linlongx/HM5_Problem6.py

--- assignment/assignment_5_linlongx/HM5_Problem6.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def rename(filename):
-    match = re.match(r"(\d{2})-(\d{2})-(\d{4})(\.\w+)", filename)
-    if match:
-        return f"{match.group(2)}-{match.group(1)}-{match.group(3)}{match.group(4)}"
-    return filename
